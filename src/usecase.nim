c5b3b0a2315809c5ccc8197b5ba691aa2403e863
limit7412 <dev@newgenerations.im>
2020-01-10 16:59:39 +0900

    date to unixtime

--- src/usecase.nim
+++ src/usecase.nim
@@ -26,6 +26,29 @@ proc translationUnixtime(self: TimeUsecase, time: int): SlackPayload =
     color: "#3f93f2",
   )])
 
+proc translationDatetime(self: TimeUsecase, date, time: string): SlackPayload =
+  let
+    datetime = parse(date & time,
+        "yyyy-MM-ddHH:mm:ss").toTime
+
+  return SlackPayload(attachments: @[SlackPost(
+    pretext:
+    "`" & date & " " & time & "` はもしJSTならUTCとunixtimeはこうで、",
+    text:
+    "JST: " & datetime.format("yyyy-MM-dd HH:mm:ss") & "\n" &
+    "UTC: " & (datetime - 9.hours).format("yyyy-MM-dd HH:mm:ss") & "\n" &
+    "unixtime: " & $(datetime.toUnix),
+    color: "#3f93f2",
+  ), SlackPost(
+    pretext:
+    "もしUTCならJSTとunixtimeはこうだよ。",
+    text:
+    "JST: " & (datetime + 9.hours).format("yyyy-MM-dd HH:mm:ss") & "\n" &
+    "UTC: " & datetime.format("yyyy-MM-dd HH:mm:ss") & "\n" &
+    "unixtime: " & $(datetime.toUnix),
+    color: "#3f93f2",
+  )])
+
 proc translation*(self: TimeUsecase, time: string): SlackPayload =
   let isUnixtime =
     try:
@@ -37,11 +60,22 @@ proc translation*(self: TimeUsecase, time: string): SlackPayload =
   if isUnixtime:
     self.translationUnixtime(time.parseInt)
   else:
-    SlackPayload(attachments: @[SlackPost(text: "string")])
+    let datetime = time.split("+")
+    case datetime.len
+    of 2:
+      self.translationDatetime(datetime[0], datetime[1].replace("%3A", ":"))
+    else:
+      SlackPayload(attachments: @[SlackPost(
+          pretext: "日時の書式が間違ってるみたい…よく確認してみて。",
+          text: "/time2unix [unixtime|yyyy-MM-dd HH:mm:ss]",
+          color: "#ffca4f",
+        )])
 
 proc err*(self: TimeUsecase, err: ref Exception) =
-  let repo = SlackRepository(url: os.getEnv("ALERT_WEBHOOK_URL").string)
-  let message = "エラーみたい…確認してみよっか"
+  let
+    repo = SlackRepository(url: os.getEnv("ALERT_WEBHOOK_URL").string)
+    message = "エラーみたい…確認してみよっか"
+
   discard repo.post(@[SlackPost(
       fallback: message,
       pretext: "<@" & os.getEnv("SLACK_ID").string & "> " & message,
